6e148933572a62dbe13adc9e0bd844560a2b4fa6
丁一娉 <dingyp@easypayx.com>
2023-10-31 12:05:16 +0800

    组件修改

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,4 +1,4 @@
-"use client";
+import React from "react";
 import { Spin } from "antd";
 
 export default function Loading() {
--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,21 +1,4 @@
-"use client";
-
 import React from "react";
-import { Button, Result } from "antd";
-
 export default function NoFound() {
-    const goHome = () => {};
-
-    return (
-        <Result
-            status="404"
-            title="404"
-            subTitle="Sorry, the page you visited does not exist."
-            extra={
-                <Button type="primary" onClick={goHome}>
-                    Back Home
-                </Button>
-            }
-        />
-    );
+    return <div>404页面</div>;
 }
